8020016f42268a9a74cbe5b23a5ac3ab8cc53f90
dolgoterdima <dolgoterdima@gmail.com>
2023-06-27 14:38:06 +0300

    feat: add prod variable

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { prodUrlPrefix, isProduction } from "@/helpers/helpers.js";
 
 const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("../pages/AllUsers.vue"),
+  },
   {
     path: "/create-user",
     name: "Create User",
     component: () => import("../pages/CreateUser.vue"),
   },
+
   {
     path: "/users",
     name: "All Users",
@@ -23,4 +30,11 @@ const router = createRouter({
   linkActiveClass: "",
   linkExactActiveClass: "nav__link--active",
 });
+/*router.beforeEach((to, from, next) => {
+  if (isProduction && !to.path.startsWith(prodUrlPrefix)) {
+    next(prodUrlPrefix + to.path);
+  } else {
+    next();
+  }
+});*/
 export default router;
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 
 import { fileURLToPath, URL } from "node:url";
+import { prodUrlPrefix } from "./src/helpers/helpers.js";
 
 export default defineConfig({
-  base: "/Accton-test-task/",
+  base: prodUrlPrefix,
   plugins: [vue(), svgLoader()],
   test: {
     environment: "happy-dom",
